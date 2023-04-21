1f307d5d582034642eeee972b810bfc2254cc4db
Olga Apenko <opomykailo@gmail.com>
2023-04-21 18:37:34 +0300

    add function to get forecast for the next 6 days

--- src/script.js
+++ src/script.js
@@ -21,45 +21,65 @@ function showCurrentDate() {
   return dayAndTime;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp*1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 let currentDay = document.querySelector('#current-day');
 currentDay.innerHTML = showCurrentDate();
 
-function displayForecast() {
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector('#forecast');
-  let days = ['Thu', 'Fri', 'Sat', 'Sun'];
+
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6 ) {forecastHTML =
       forecastHTML +
       `
    <div class="col-2">
-    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
     <img
-      src="http://openweathermap.org/img/wn/50d@2x.png"
+      src="http://openweathermap.org/img/wn/${
+        forecastDay.weather[0].icon
+      }@2x.png"
       alt=""
       width="42"
     />
     <div class="weather-forecast-temperatures">
-      <span class="weather-forecast-temperature-max"> 18° </span>
-      <span class="weather-forecast-temperature-min"> 12° </span>
+      <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+      <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
     </div>
   </div>
     `;
+    }
   });
 
-  forecastHTML = forecastHTML + `</div>`;
+  forecastHTML =forecastHTML+ `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 
-// Weather API
+
+function getForecast(coordinates) {
+  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 
 function showTemperature(response) {
   document.querySelector('#current-location').innerHTML = response.data.name;
-  document.querySelector('#temperature').innerHTML = Math.round(
-    response.data.main.temp
-  );
 
   celciusTemperature = response.data.main.temp;
+
+  document.querySelector('#temperature').innerHTML = Math.round(
+    celciusTemperature
+  );
 
   document.querySelector('#description').innerHTML =
     response.data.weather[0].main;
@@ -76,6 +96,8 @@ function showTemperature(response) {
   document
     .querySelector('#icon')
     .setAttribute('alt', response.data.weather[0].description);
+
+    getForecast(response.data.coord);
 }
 
 function searchCity(city) {
